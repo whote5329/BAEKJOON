1582dd23f7a2addf66d4cd251dafea99f3216490
whote5329 <80526394+whote5329@users.noreply.github.com>
2022-01-13 22:36:51 +0900

    1110_더하기 사이클

---
+++ baekjoon1110/baekjoon1110/source.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+	int a, b, c, d = -1, result, input, count = 0;
+
+	scanf_s("%d", &input);
+	result = input;
+
+	while (d != result) {
+		a = input / 10;
+		b = input % 10;
+		c = (a + b) % 10;
+		d = (b * 10) + c;
+		input = d;
+		count++;
+	}
+	printf("%d", count);
+}
